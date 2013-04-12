5db22e737b143b1aa862791dcdbbbd825cee0b04
Joshua Szmajda <josh@optoro.com>
2013-04-12 17:34:51 -0400

    bare rails install w/ rvm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madlibs::Application.config.secret_token = '49be88fd3e84dba63113f6be047855f2dc8b7ae2ecdea7b820c26fbf46b9b2c32e3e123b6fa29a763127b1e51c5eec52ecce5db7d774ab84a8ac9863b61500fd'
