0f82d61e17f1b3270aa8076cb03b414e15e333cd
Zack Frank <frank.zack@gmail.com>
2018-11-07 00:27:10 -0600

    Update migrations with new db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106022154) do
+ActiveRecord::Schema.define(version: 20181107062037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 20181106022154) do
   create_table "solutions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "mad_lib_id"
     t.string "text"
+    t.integer "mad_lib_id"
   end
 
 end
